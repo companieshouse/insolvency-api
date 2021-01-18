2cd98f7c72102e45b8a4b2ff024f9872ee4c2222
rsachdevch <rsachdev@companieshouse.gov.uk>
2021-01-18 15:45:56 +0000

    Add healthcheck and register methods

---
+++ handlers/register.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+  "github.com/companieshouse/chs.go/log"
+  "github.com/gorilla/mux"
+	"net/http"
+)
+
+func Register(mainRouter *mux.Router) {
+  mainRouter.HandleFunc("/insolvency-service/healthcheck", healthCheck).Methods(http.MethodGet).Name("healthcheck")
+	mainRouter.Use(log.Handler)
+}
+
+func healthCheck(w http.ResponseWriter, _ *http.Request) {
+  w.WriteHeader(http.StatusOK)
+}
