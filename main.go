39fc818bcb2569167d5500da7f0ec39f1283d72f
rsachdevch <rsachdev@companieshouse.gov.uk>
2021-01-18 15:45:41 +0000

    Add main file

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/companieshouse/chs.go/log"
+	"github.com/companieshouse/insolvency-api/config"
+	"github.com/companieshouse/insolvency-api/handlers"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	namespace := "insolvency-api"
+	log.Namespace = namespace
+
+	// Get environment config for app
+	cfg, err := config.Get()
+	if err != nil {
+		log.Error(fmt.Errorf("error configuring service: %s. Exiting", err), nil)
+		return
+	}
+
+	// Create router
+	mainRouter := mux.NewRouter()
+
+	handlers.Register(mainRouter)
+
+	log.Info("Starting " + namespace)
+
+	h := &http.Server{
+		Addr:    cfg.BindAddr,
+		Handler: mainRouter,
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	// run server in new go routine to allow app shutdown signal wait below
+	go func() {
+		log.Info("starting server...", log.Data{"port": cfg.BindAddr})
+		err = h.ListenAndServe()
+		log.Info("server stopping...")
+		if err != nil && err != http.ErrServerClosed {
+			log.Error(err)
+			os.Exit(1)
+		}
+	}()
+
+	// wait for app shutdown message before attempting to close server gracefully
+	<-stop
+
+	log.Info("shutting down server...")
+	timeout := time.Duration(5) * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	err = h.Shutdown(ctx)
+	if err != nil {
+		log.Error(fmt.Errorf("failed to shutdown server gracefully: [%v]", err))
+	} else {
+		log.Info("server shutdown gracefully")
+	}
+}
