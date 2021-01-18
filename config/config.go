07273cb33ab625c9cfe3cf141bce9fe2f9e9daf0
rsachdevch <rsachdev@companieshouse.gov.uk>
2021-01-18 15:45:18 +0000

    Add config

---
+++ config/config.go
@@ -0,0 +1,32 @@
+// Package config defines the environment variable and command-line flags
+package config
+
+import (
+	"github.com/companieshouse/gofigure"
+	"sync"
+)
+
+var cfg *Config
+var mtx sync.Mutex
+
+type Config struct {
+	BindAddr string `env:"BIND_ADDR" flag:"bind-addr" flagDesc:"Bind address"`
+}
+
+func Get() (*Config, error) {
+	mtx.Lock()
+	defer mtx.Unlock()
+
+	if cfg != nil {
+		return cfg, nil
+	}
+
+	cfg = &Config{}
+
+	err := gofigure.Gofigure(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
