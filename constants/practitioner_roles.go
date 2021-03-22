8061f9a770d3d08c33b4ee99cd711137b05ff905
rsachdevch <rsachdev@companieshouse.gov.uk>
2021-03-22 08:57:33 +0000

    Add comments to exported functions

--- constants/practitioner_roles.go
+++ constants/practitioner_roles.go
@@ -26,10 +26,13 @@ var practitionerRoles = [...]string{
 	"interim-liquidator",
 }
 
+// String returns the correctly formatted practitioner role
 func (practitionerRole PractitionerRole) String() string {
 	return practitionerRoles[practitionerRole-1]
 }
 
+// IsInRoleList checks if the roleName string supplied is a valid string by comparing
+// it to the list of accepted practitioner roles
 func IsInRoleList(roleName string) bool {
 	for _, v := range practitionerRoles {
 		if roleName == v {
