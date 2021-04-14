af8cab4cf412106ba5596ef2195021839e3eeaa5
rsachdevch <rsachdev@companieshouse.gov.uk>
2021-04-14 13:46:08 +0100

    Remove _ from id

--- models/data_entities.go
+++ models/data_entities.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // InsolvencyResourceDao contains the meta-data for the insolvency resource in Mongo
 type InsolvencyResourceDao struct {
-	ID            primitive.ObjectID         `bson:"_id"`
+	ID            primitive.ObjectID         `bson:"id"`
 	TransactionID string                     `bson:"transaction_id"`
 	Etag          string                     `bson:"etag"`
 	Kind          string                     `bson:"kind"`
