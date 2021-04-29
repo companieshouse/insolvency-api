5e177dec2f658be9331bf9b7dbdb147b5dfc68c0
Joseph-Nash <jnash@companieshouse.gov.uk>
2021-04-29 11:57:23 +0100

    validate telephone number as number between 10-11 digits

--- models/request_entities.go
+++ models/request_entities.go
@@ -12,7 +12,7 @@ type PractitionerRequest struct {
 	IPCode          string  `json:"ip_code" validate:"required,number,max=8"`
 	FirstName       string  `json:"first_name" validate:"required"`
 	LastName        string  `json:"last_name" validate:"required"`
-	TelephoneNumber string  `json:"telephone_number"`
+	TelephoneNumber string  `json:"telephone_number" validate:"omitempty,min=10,max=11,number"`
 	Email           string  `json:"email" validate:"omitempty,email"`
 	Address         Address `json:"address" validate:"required"`
 	Role            string  `json:"role" validate:"required"`
