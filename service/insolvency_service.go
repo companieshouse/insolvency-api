4a8ebcb9787db64d01a8ebc0764999167dba0dd6
rsachdevch <rsachdev@companieshouse.gov.uk>
2021-05-20 12:21:42 +0100

    Add validation rule to check if all or no practitioners are appointed

--- service/insolvency_service.go
+++ service/insolvency_service.go
@@ -15,13 +15,31 @@ func ValidateInsolvencyDetails(svc dao.Service, transactionID string) (bool, *[]
 	validationErrors := make([]models.ValidationErrorResponseResource, 0)
 
 	// Retrieve details for the insolvency resource from DB
-	_, err := svc.GetInsolvencyResource(transactionID)
+	insolvencyResource, err := svc.GetInsolvencyResource(transactionID)
 	if err != nil {
 		log.Error(fmt.Errorf("error getting insolvency resource from DB [%s]", err))
 		validationErrors = addValidationError(validationErrors, fmt.Sprintf("error getting insolvency resource from DB: [%s]", err), "insolvency case")
 
 		// If there is an error retrieving the insolvency resource return without running any other validations as they will all fail
 		return false, &validationErrors
+	}
+
+	// Check if there is one practitioner appointed and if there is, ensure that all practitioners are appointed
+	hasAppointedPractitioner := false
+	for _, practitioner := range insolvencyResource.Data.Practitioners {
+		if practitioner.Appointment != nil {
+			hasAppointedPractitioner = true
+		}
+	}
+	if hasAppointedPractitioner {
+		for _, practitioner := range insolvencyResource.Data.Practitioners {
+			if practitioner.Appointment == nil || practitioner.Appointment.AppointedOn == "" {
+				validationError := fmt.Sprintf("error - all practitioners for insolvency case with transaction id [%s] have been not appointed", insolvencyResource.TransactionID)
+				log.Error(fmt.Errorf(validationError))
+				validationErrors = addValidationError(validationErrors, validationError, "appointment")
+				return false, &validationErrors
+			}
+		}
 	}
 
 	//TODO: Validation checks for insolvency case
