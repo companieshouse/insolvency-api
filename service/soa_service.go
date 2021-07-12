471a05083e6fa9aabc888ad93f1dbdbaa067ee2c
Joseph-Nash <jnash@companieshouse.gov.uk>
2021-07-12 14:19:24 +0100

    add soa service and tests for validation

---
+++ service/soa_service.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/companieshouse/chs.go/log"
+	"github.com/companieshouse/insolvency-api/dao"
+	"github.com/companieshouse/insolvency-api/models"
+	"github.com/companieshouse/insolvency-api/utils"
+)
+
+// ValidateStatementOfAffairsRequest checks that the incoming statement of affairs request is valid
+func ValidateStatementOfAffairsRequest(statement models.StatementOfAffairs) string {
+	var errs []string
+
+	// Check that the attachment has been submitted correctly
+	if len(statement.Attachments) == 0 || len(statement.Attachments) > 1 {
+		errs = append(errs, "please supply only one attachment")
+	}
+	return strings.Join(errs, ", ")
+}
+
+// ValidateStatementDate checks that the incoming statement date is valid
+func ValidateStatementDate(svc dao.Service, statementDao *models.StatementOfAffairsResourceDao, transactionID string, req *http.Request) (string, error) {
+	var errs []string
+
+	// Check if statement date supplied is in the future or before company was incorporated
+	insolvencyResource, err := svc.GetInsolvencyResource(transactionID)
+	if err != nil {
+		err = fmt.Errorf("error getting insolvency resource from DB: [%s]", err)
+		log.ErrorR(req, err)
+		return "", err
+	}
+	// Retrieve company incorporation date
+	incorporatedOn, err := GetCompanyIncorporatedOn(insolvencyResource.Data.CompanyNumber, req)
+	if err != nil {
+		err = fmt.Errorf("error getting company details from DB: [%s]", err)
+		log.ErrorR(req, err)
+		return "", err
+	}
+
+	ok, err := utils.IsValidDate(statementDao.StatementDate, incorporatedOn)
+	if err != nil {
+		err = fmt.Errorf("error parsing date: [%s]", err)
+		log.ErrorR(req, err)
+		return "", err
+	}
+	if !ok {
+		errs = append(errs, fmt.Sprintf("statement_date [%s] should not be in the future or before the company was incorporated", statementDao.StatementDate))
+	}
+
+	return strings.Join(errs, ", "), nil
+}
---
+++ service/soa_service_test.go
@@ -0,0 +1,182 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/companieshouse/insolvency-api/mocks"
+	"github.com/companieshouse/insolvency-api/models"
+	"github.com/golang/mock/gomock"
+	"github.com/jarcoal/httpmock"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestUnitIsValidStatementOfAffairsRequest(t *testing.T) {
+	Convey("Statement of Affairs request supplied is invalid - no attachment has been supplied", t, func() {
+		statement := generateStatement()
+		statement.Attachments = []string{}
+
+		err := ValidateStatementOfAffairsRequest(models.StatementOfAffairs(statement))
+
+		So(err, ShouldNotBeBlank)
+		So(err, ShouldContainSubstring, "please supply only one attachment")
+	})
+
+	Convey("Statement of Affairs request supplied is invalid - more than one attachment has been supplied", t, func() {
+		statement := generateStatement()
+		statement.Attachments = []string{
+			"1234567890",
+			"0987654321",
+		}
+
+		err := ValidateStatementOfAffairsRequest(models.StatementOfAffairs(statement))
+
+		So(err, ShouldNotBeBlank)
+		So(err, ShouldContainSubstring, "please supply only one attachment")
+	})
+}
+
+func TestUnitIsValidStatementDate(t *testing.T) {
+	transactionID := "123"
+	apiURL := "https://api.companieshouse.gov.uk"
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	Convey("error retrieving insolvency resource", t, func() {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockService := mocks.NewMockService(mockCtrl)
+		mockService.EXPECT().GetInsolvencyResource(transactionID).Return(models.InsolvencyResourceDao{}, fmt.Errorf("err"))
+
+		statement := generateStatement()
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		validationErr, err := ValidateStatementDate(mockService, &statement, transactionID, req)
+		So(err.Error(), ShouldContainSubstring, "err")
+		So(validationErr, ShouldBeEmpty)
+	})
+
+	Convey("error retrieving company details", t, func() {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		defer httpmock.Reset()
+		httpmock.RegisterResponder(http.MethodGet, apiURL+"/company/1234", httpmock.NewStringResponder(http.StatusTeapot, ""))
+
+		mockService := mocks.NewMockService(mockCtrl)
+		mockService.EXPECT().GetInsolvencyResource(transactionID).Return(generateInsolvencyResource(), nil)
+
+		statement := generateStatement()
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		validationErr, err := ValidateStatementDate(mockService, &statement, transactionID, req)
+		So(validationErr, ShouldBeEmpty)
+		So(err.Error(), ShouldContainSubstring, "error getting company details from DB")
+	})
+
+	Convey("error parsing appointedOn date", t, func() {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		defer httpmock.Reset()
+		httpmock.RegisterResponder(http.MethodGet, apiURL+"/company/1234", httpmock.NewStringResponder(http.StatusOK, companyProfileDateResponse("2000-06-26 00:00:00.000Z")))
+
+		mockService := mocks.NewMockService(mockCtrl)
+		mockService.EXPECT().GetInsolvencyResource(transactionID).Return(generateInsolvencyResource(), nil)
+
+		statement := generateStatement()
+		statement.StatementDate = "2001/1/2"
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		validationErr, err := ValidateStatementDate(mockService, &statement, transactionID, req)
+		So(validationErr, ShouldBeEmpty)
+		So(err.Error(), ShouldContainSubstring, "error parsing date")
+	})
+
+	Convey("error parsing incorporatedOn date", t, func() {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		defer httpmock.Reset()
+		httpmock.RegisterResponder(http.MethodGet, apiURL+"/company/1234", httpmock.NewStringResponder(http.StatusOK, companyProfileDateResponse("error")))
+
+		statement := generateStatement()
+		mockService := mocks.NewMockService(mockCtrl)
+		mockService.EXPECT().GetInsolvencyResource(transactionID).Return(generateInsolvencyResource(), nil)
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		validationErr, err := ValidateStatementDate(mockService, &statement, transactionID, req)
+		So(validationErr, ShouldBeEmpty)
+		So(err.Error(), ShouldContainSubstring, "error parsing date")
+	})
+
+	Convey("invalid date - in the future", t, func() {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		defer httpmock.Reset()
+		httpmock.RegisterResponder(http.MethodGet, apiURL+"/company/1234", httpmock.NewStringResponder(http.StatusOK, companyProfileDateResponse("2000-06-26 00:00:00.000Z")))
+
+		mockService := mocks.NewMockService(mockCtrl)
+		mockService.EXPECT().GetInsolvencyResource(transactionID).Return(generateInsolvencyResource(), nil)
+
+		statement := generateStatement()
+		statement.StatementDate = time.Now().AddDate(0, 0, 1).Format("2006-01-02")
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		validationErr, err := ValidateStatementDate(mockService, &statement, transactionID, req)
+		So(validationErr, ShouldContainSubstring, "should not be in the future")
+		So(err, ShouldBeNil)
+	})
+
+	Convey("invalid appointedOn date - before company was incorporated", t, func() {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		defer httpmock.Reset()
+		httpmock.RegisterResponder(http.MethodGet, apiURL+"/company/1234", httpmock.NewStringResponder(http.StatusOK, companyProfileDateResponse("2000-06-26 00:00:00.000Z")))
+
+		mockService := mocks.NewMockService(mockCtrl)
+		mockService.EXPECT().GetInsolvencyResource(transactionID).Return(generateInsolvencyResource(), nil)
+
+		statement := generateStatement()
+		statement.StatementDate = "1999-01-01"
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		validationErr, err := ValidateStatementDate(mockService, &statement, transactionID, req)
+		So(validationErr, ShouldContainSubstring, "before the company was incorporated")
+		So(err, ShouldBeNil)
+	})
+
+	Convey("valid date", t, func() {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		defer httpmock.Reset()
+		httpmock.RegisterResponder(http.MethodGet, apiURL+"/company/1234", httpmock.NewStringResponder(http.StatusOK, companyProfileDateResponse("2000-06-26 00:00:00.000Z")))
+
+		mockService := mocks.NewMockService(mockCtrl)
+		mockService.EXPECT().GetInsolvencyResource(transactionID).Return(generateInsolvencyResource(), nil)
+
+		statement := generateStatement()
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		validationErr, err := ValidateStatementDate(mockService, &statement, transactionID, req)
+		So(validationErr, ShouldBeEmpty)
+		So(err, ShouldBeNil)
+	})
+
+}
+
+func generateStatement() models.StatementOfAffairsResourceDao {
+	return models.StatementOfAffairsResourceDao{
+		StatementDate: "2012-01-23",
+		Attachments: []string{
+			"123456789",
+		},
+	}
+}
