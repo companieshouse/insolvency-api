f0eaede4ce4d9a3d241bdeb6f4497573c021c640
Edwin Mead <emead@companieshouse.gov.uk>
2021-11-25 15:20:39 +0000

    Remove incorrect and unnecessary comments

--- service/efs_submission_service.go
+++ service/efs_submission_service.go
@@ -9,16 +9,13 @@ import (
 
 // IsUserOnEfsAllowList uses the sdk to call the EFS api and return a boolean depending on whether or not the email address is on the allow list
 func IsUserOnEfsAllowList(emailAddress string, req *http.Request) (bool, error) {
-	// Create Private SDK session
 	api, err := manager.GetPrivateSDK(req)
 	if err != nil {
 		return false, fmt.Errorf("error creating SDK to call transaction api: [%v]", err.Error())
 	}
 
-	// Patch transaction api with insolvency resource
 	isUserAllowed, err := api.Efs.IsUserOnAllowList(emailAddress).Do()
 	if err != nil {
-		// Return that there has been an error contacting the transaction api
 		return false, fmt.Errorf("error communicating with the EFS submission api: [%s]", err)
 	}
 
